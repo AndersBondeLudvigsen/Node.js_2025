afc6482b25447f1b5af7e011867cc2e1f75926f6
AndersBondeLudvigsen <144429290+AndersBondeLudvigsen@users.noreply.github.com>
2025-02-14 13:47:20 +0100

    post mapping

--- 02._First_Server/app.js
+++ 02._First_Server/app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const app =  express();
+app.use(express.json());
 
 app.get("/", (req, res) =>{
 res.send({ data: "This is the root route" });
@@ -9,6 +10,12 @@ app.get("/blablabla", (req, res) =>{
     res.send({ data: "This is the blablabla route" });
     });
 
+
+console.log(__dirname)
+
+ app.get("/welcomepage", (req,res) =>{
+    res.sendFile(__dirname + `/index.html`);
+ })   
 
    
 app.get("/blablablabla", (req, res) =>{
@@ -46,6 +53,36 @@ app.get("/favorite/:flower/:animal", functionRefrenceFlowerAndAnimal);
 app.get("/favoritethings/:flower/:animal", (req, res) => {
     res.send({ data: `Favorite things are ${req.params.flower} and favorite animal is ${req.params.animal}` });
 });
+
+
+
+
+// Sende data med querystring og pathvaruble med GET
+
+app.get("/search" , (req,res)=>{
+    console.log(req.query)
+    res.send({ data: `You have seached for: ${req.query.q}`})
+});
+
+
+
+
+app.post("/favoritepoliticians",(req,res) =>{
+    console.log(req.body);
+  
+    res.send( {data:req.body} )
+});
+
+
+
+// assignment create a new route and send some data to it throgh the body
+
+app.post("/aleksandersmor", (req,res) =>{
+    console.log(req.body)
+    res.send({data : req.body})
+})
+
+
 
 
 app.listen(8080);
