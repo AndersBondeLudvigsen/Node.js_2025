34e0e68cf8c004a4f97012b98c439225cf797f5c
AndersBondeLudvigsen <144429290+AndersBondeLudvigsen@users.noreply.github.com>
2025-03-21 11:12:11 +0100

    Finished first svelte

---
+++ 07._First_Svelte_Project/src/components/Messages/Messages.svelte
@@ -0,0 +1,19 @@
+<script>
+    let messages = $state([]);
+
+    let messageInput = $state("");
+
+    function addMessage (){
+        messages.push(messageInput)
+        messageInput = ""
+    }
+
+</script>
+
+<h1>Messages</h1>
+
+<p>{messages}</p>
+
+<input bind:value={messageInput} placeholder="Type your message...">
+
+<button onclick={addMessage}>Submit Message</button>
